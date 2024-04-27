51159da358c0c40d1452c437de74d9848969880b
akromjon <numonov.akromjon1998@gmail.com>
2024-04-27 23:31:19 +0500

    feature: all ints in rust

---
+++ src/integers.rs
@@ -0,0 +1,52 @@
+pub fn run_i() {
+    let i_8_min: i8 = i8::MIN;
+    let i_8_max: i8 = i8::MAX;
+
+    let i_16_min: i16=i16::MIN; 
+    let i_16_max: i16=i16::MAX;
+
+    let i_32_min: i32=i32::MIN;
+    let i_32_max: i32=i32::MAX;
+
+    let i_64_min: i64=i64::MIN;
+    let i_64_max: i64=i64::MAX;
+
+    let i_128_min: i128=i128::MIN;
+    let i_128_max: i128=i128::MAX;
+
+    println!("i_8: {} - {}",i_8_min,i_8_max);
+    println!("i_16: {} - {}",i_16_min,i_16_max);
+
+    println!("i_32: {} - {}",i_32_min,i_32_min);
+    println!("i_32: {} - {}",i_32_min,i_32_max);
+
+    println!("i_64: {} - {}",i_64_min,i_64_max);
+    println!("i_128: {} - {}",i_128_min,i_128_max);
+
+}
+pub fn run_u() {
+    let u_8_min: u8 = u8::MIN;
+    let u_8_max: u8 = u8::MAX;
+
+    let u_16_min: u16=u16::MIN; 
+    let u_16_max: u16=u16::MAX;
+
+    let u_32_min: u32=u32::MIN;
+    let u_32_max: u32=u32::MAX;
+
+    let u_64_min: u64=u64::MIN;
+    let u_64_max: u64=u64::MAX;
+
+    let u_128_min: u128=u128::MIN;
+    let u_128_max: u128=u128::MAX;
+
+    println!("u_8: {} - {}",u_8_min,u_8_max);
+    println!("u_16: {} - {}",u_16_min,u_16_max);
+
+    println!("u_32: {} - {}",u_32_min,u_32_min);
+    println!("u_32: {} - {}",u_32_min,u_32_max);
+
+    println!("u_64: {} - {}",u_64_min,u_64_max);
+    println!("u_128: {} - {}",u_128_min,u_128_max);
+
+}
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod integers;
+fn main() {
+    
+    integers::run_i();
+    
+    println!("\n");
+    
+    integers::run_u();
+
+    println!("\ngithub.com/akromjon/rust-integers");
+
+}
